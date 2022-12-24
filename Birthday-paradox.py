2803d41894e4f7fad95085569be90cbad398c2e4
Cairo Cananea <46378837+cacaprog@users.noreply.github.com>
2022-12-24 13:55:46 -0300

    Code complete. But I will test it now

--- Birthday-paradox.py
+++ Birthday-paradox.py
@@ -48,6 +48,42 @@ for i, birthday in enumerate(birthdays):
 print()
 print()
 
+# Determine if there are two birthdays that match
+match = getMatch(birthdays)
+
+# Display the results
+print('In this simulation, ', end='')
+if match != None:
+    monthName = MONTHS[match.month - 1]
+    dateText = '{} {}'.format(monthName, match.day)
+    print('multiple people have a birthday on', dateText)
+else:
+    print('the are no matching birthdays.')
+print()
+
+# Run throught 100.000 simulations:
+print('Generating', numBDays, 'random birthdays 100.000 times...')
+input('Press Enter to begin...')
+
+print('Let\'s run another 100.000 simulations.')
+simMatch = 0 # How many simulations had matching birthdays in them
+for i in range(100_000):
+    # report the progress every 10.000 simulations
+    if i % 10_000 == 0:
+        print(1, 'simulations run...')
+    birthdays = getBirthdays(numBDays)
+    if getMatch(birthdays) != None:
+        simMatch = simMatch + 1
+print('100.000 simulations run.')
+
+# Display simulations result
+probability = round(simMatch / 100_000 * 100, 2)
+print('Out of 100.000 simulations of', numBDays, 'people, there was a ')
+print('matching birthday in that group', simMatch, 'times. This means')
+print('that', numBDays, 'people have a', probability, '% chance of')
+print('having a matching birthday in their group.')
+print('That\'s probably more than you would think!')
+
 
 
 
