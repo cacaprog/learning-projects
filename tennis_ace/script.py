00a3452cb373a5dec881ee760a4f7e200c491a8f
cacaprog <cacaprog@gmail.com>
2023-02-16 13:13:10 -0300

    tennis ace project for linear regression upload

---
+++ tennis_ace/script.py
@@ -0,0 +1,85 @@
+import codecademylib3_seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+# load and investigate the data here:
+
+
+
+
+
+
+
+
+# perform exploratory analysis here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform single feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform two feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform multiple feature linear regressions here:
