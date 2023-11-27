807b2d418bf03407f3b1caa26e8fb4057f767e6c
cacaprog <ccananea@yahoo.com.br>
2023-11-27 14:23:46 -0300

    mlzoomcamp week 9 homework

---
+++ ml-zoomcamp/homework/09_serverless/homework.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import tensorflow.lite as tflite
+import tflite_runtime.interpreter as tflite
+import numpy as np
+
+import os
+
+from io import BytesIO
+from urllib import request
+
+from PIL import Image
+
+MODEL_NAME = os.getenv('MODEL_NAME', 'bees-wasps.tflite')
+
+interpreter = tflite.Interpreter(model_path='bees-wasps.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+# url = 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'
+
+def prepare_input(x):
+    return x / 255.0
+
+
+def predict(url):
+    img = download_image(url)
+    img = prepare_image(img, (150, 150))
+
+    x = np.array(img, dtype='float32')
+    X = np.array([x])
+
+    X = prepare_input(X)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+
+    preds = interpreter.get_tensor(output_index)
+
+    return float(preds[0, 0])
+
+
+def lambda_handler(event, context):
+    url = event['url']
+    pred = predict(url)
+    result = {
+        'prediction': pred
+    }
+
+    return result
---
+++ ml-zoomcamp/homework/09_serverless/test.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'http://localhost:8080'
+
+data = {'url': 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'}
+
+result = requests.post(url, json=data).json()
+print(result)
